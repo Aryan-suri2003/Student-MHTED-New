d1b81fef2f5cd5ba9a6f4ddbef81526a498cd6e1
Ayushitiwari01 <tiwariayushi0107@gmail.com>
2026-08-17 18:15:07 +0530

    Save current Ayushi library work

--- src/app/libraries/page.tsx
+++ src/app/libraries/page.tsx
@@ -1,9 +1,5 @@
-import App from '@/App';
+import LibraryDashboardClient from '@/components/LibraryDashboardClient';
 
 export default function LibrariesPage() {
-  return (
-    <div className="[&>div>header]:hidden [&>div>footer]:hidden">
-      <App />
-    </div>
-  );
+  return <LibraryDashboardClient />;
 }
