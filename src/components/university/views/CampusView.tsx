e46b517ea35418f07e347a28b7b9504ecc2f6860
Mukprojects <mukundnews.chnd@gmail.com>
2026-08-18 15:12:16 +0530

    Run Build

--- src/components/university/views/CampusView.tsx
+++ src/components/university/views/CampusView.tsx
@@ -35,7 +35,7 @@ export const CampusView: React.FC<CampusViewProps> = ({
 
   const itemVariants = {
     hidden: { opacity: 0, y: 15, scale: 0.95 },
-    show: { opacity: 1, y: 0, scale: 1, transition: { type: 'spring', stiffness: 300, damping: 24 } }
+    show: { opacity: 1, y: 0, scale: 1, transition: { type: 'spring' as const, stiffness: 300, damping: 24 } }
   };
 
   return (
